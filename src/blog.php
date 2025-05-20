be7583857dbe037c8d6019d1aa9b3cf0cb4d7337
Hernan-Leggo <hernanleguizamondg02@gmail.com>
2025-05-19 21:21:42 -0300

    cambio de log

--- src/blog.php
+++ src/blog.php
@@ -1,5 +1,5 @@
 <?php include "includes/header.php"; ?>
 <h2>Blog del Bienestar</h2>
-<iframe src="https://bienestare-estudiantil.blogspot.com/" width="100%" height="500px"></iframe>
-<p>Autor: Giovanni Cáceres</p>
+<iframe src="https://bienestaruniversitariohdlg.blogspot.com/2025/05/10-consejos-para-estudiantes.html" width="100%" height="500px"></iframe>
+<p>Autor: Hernan Leguizamon</p>
 <?php include "includes/footer.php"; ?>
--- src/includes/footer.php
+++ src/includes/footer.php
@@ -1,7 +1,7 @@
 </main>
 <footer>
     <p>© 2025 - Ingeniería Informática - Grupo: Giovanni Cáceres, Hernan Leguizamon, Kristian Benitez, Sergio López, Henry Ramírez</p>
-    <p>Autor del entregable: Giovanni Cáceres</p>
+    <p>Autor del entregable: Hernan Leguizamon</p>
 </footer>
 </body>
 </html>
